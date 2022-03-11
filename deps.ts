ceeab8542cfb5e67424dfad2f494115e1f2c800d
ThatGuyJamal <43446907+ThatGuyJamal@users.noreply.github.com>
2022-03-11 12:21:08 -0500

    Update deps.ts
    Fixing deps versions

--- deps.ts
+++ deps.ts
@@ -1,2 +1,8 @@
 // Main code from discordeno
-export * from "https://deno.land/x/discordeno@13.0.0-rc20/mod.ts";
+export * from "https://deno.land/x/discordeno@13.0.0-rc18/mod.ts";
+export * from "https://deno.land/x/discordeno_cache_plugin@0.0.21/mod.ts";
+export * from "https://deno.land/x/discordeno_permissions_plugin@0.0.15/mod.ts";
+export * from "https://deno.land/x/discordeno_helpers_plugin@0.0.8/mod.ts";
+
+// util
+export * from "https://deno.land/std@0.117.0/fmt/colors.ts";
--- packages/core/interfaces/Monitor.ts
+++ packages/core/interfaces/Monitor.ts
@@ -1,4 +1,4 @@
-import { DiscordenoMessage, PermissionStrings } from "https://deno.land/x/discordeno@13.0.0-rc20/mod.ts";
+import { DiscordenoMessage, PermissionStrings } from "../../../deps.ts";
 
 export interface Monitor {
   /** The name of the monitor */
