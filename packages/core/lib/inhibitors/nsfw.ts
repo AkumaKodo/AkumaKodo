188ef36dfb85741fdc8ad8d9c05beefe390d2b52
ThatGuyJamal <43446907+ThatGuyJamal@users.noreply.github.com>
2022-03-11 16:55:53 -0500

    Create nsfw.ts

---
+++ packages/core/lib/inhibitors/nsfw.ts
@@ -0,0 +1,20 @@
+import {AkumaKomoBot} from "../AkumaKodo.ts";
+
+
+AkumaKomoBot.inhibitor.set('nsfw', (message, command) => {
+    // Returns false so the command can be executed if the command is not NSFW
+    if(!command.nsfw) return false
+
+    // DMs are not channels, so we need to return true to cancel the command on dm events
+    if(!message.guildId) return true
+
+    // check if channel is NSFW
+    const channel = AkumaKomoBot.channels.get(message.channelId)
+
+    if(channel) {
+        return channel.nsfw
+    } else {
+        return true
+    }
+
+})
