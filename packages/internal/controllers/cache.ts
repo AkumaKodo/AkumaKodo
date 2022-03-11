7b516912db6f96104d600abee0fc7fbd04f2677e
ThatGuyJamal <43446907+ThatGuyJamal@users.noreply.github.com>
2022-03-11 11:54:48 -0500

    Create cache.ts

---
+++ packages/internal/controllers/cache.ts
@@ -0,0 +1,18 @@
+import { AkumaKomoBotWithCache } from "../../core/lib/mod.ts";
+
+/**
+ * CacheController
+ * @author ThatGuyJamal
+ */
+export class CacheController {
+  public channels = AkumaKomoBotWithCache.channels;
+  public guilds = AkumaKomoBotWithCache.guilds;
+  public users = AkumaKomoBotWithCache.members;
+  public messages = AkumaKomoBotWithCache.messages;
+  public presences = AkumaKomoBotWithCache.presences;
+  public dispatchedGuildIds = AkumaKomoBotWithCache.dispatchedGuildIds;
+  public dispatchedChannelIds = AkumaKomoBotWithCache.dispatchedChannelIds;
+  public activeGuildIds = AkumaKomoBotWithCache.activeGuildIds;
+
+  
+}
