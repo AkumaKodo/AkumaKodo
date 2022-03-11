e75ead6a22f0c5a33e09ebf671df462843099960
ThatGuyJamal <43446907+ThatGuyJamal@users.noreply.github.com>
2022-03-11 14:31:25 -0500

    Update AkumaKodoUtils.ts

--- packages/core/lib/AkumaKodoUtils.ts
+++ packages/core/lib/AkumaKodoUtils.ts
@@ -1,11 +1,11 @@
-import {AkumaCreateBotOptions} from "./AkumaKodo.ts";
+import { AkumaCreateBotOptions } from "./AkumaKodo.ts";
 
 /**
  * Helps validate the options passed to the AkumaKodoBot constructor.
  * @param o The options to validate.
  */
-export function validateCreateBotOptions (o?: AkumaCreateBotOptions) {
-    if(!o.token) {
-        throw new Error("Missing token");
-    }
+export function validateCreateBotOptions(o?: AkumaCreateBotOptions) {
+  if (!o.token) {
+    throw new Error("Missing token");
+  }
 }
