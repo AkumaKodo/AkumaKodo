611959cdae2c30011124daa8e1f384e29ddbc19e
ThatGuyJamal <43446907+ThatGuyJamal@users.noreply.github.com>
2022-03-12 02:01:02 -0500

    Update common.ts

--- packages/core/lib/arguments/common.ts
+++ packages/core/lib/arguments/common.ts
@@ -1,6 +1,8 @@
 import { AkumaKomoBot } from "../AkumaKodo.ts";
 import { stringToMilliseconds } from "../utils/Helpers.ts";
 
+const SNOWFLAKE_REGEX = /[0-9]{17,19}/;
+
 AkumaKomoBot.argumentsCollection.set("boolean", {
   name: "boolean",
   execute: function (_, params) {
@@ -15,7 +17,6 @@ AkumaKomoBot.argumentsCollection.set("boolean", {
 AkumaKomoBot.argumentsCollection.set("snowflake", {
   name: "snowflake",
   execute: function (_, params) {
-    const SNOWFLAKE_REGEX = /[0-9]{17,19}/;
     let [text] = params;
     if (!text) return;
     // If it's a nickname mention or role mention
@@ -41,4 +42,27 @@ AkumaKomoBot.argumentsCollection.set("duration", {
 
     return stringToMilliseconds(time);
   },
+});
+
+AkumaKomoBot.argumentsCollection.set('...snowflakes', {
+  name: '...snowflakes',
+  execute: function (_, params) {
+    const cleaned = params.map((p) => {
+      // If its just a normal id number
+      if (!p.startsWith("<")) return p;
+      // If its a nickname mention or role mention
+      if (p.startsWith("<@!") || p.startsWith("<@&")) {
+        return p.substring(3, p.length - 1);
+      }
+      // If it's a user mention or channel mention
+      if (p.startsWith("<@") || p.startsWith("<#")) {
+        return p.substring(2, p.length - 1);
+      }
+
+      // Unknown
+      return p;
+    });
+
+    return cleaned.filter((text) => SNOWFLAKE_REGEX.test(text));
+  }
 });
