8847fe21682d8c9e27ecdce37ed752f6a0e3a44e
ThatGuyJamal <43446907+ThatGuyJamal@users.noreply.github.com>
2022-03-12 20:10:13 -0500

    Update deps.ts

--- deps.ts
+++ deps.ts
@@ -7,7 +7,10 @@ export {
   enableCacheSweepers,
 } from "https://deno.land/x/discordeno_cache_plugin@0.0.21/mod.ts";
 export { enablePermissionsPlugin } from "https://deno.land/x/discordeno_permissions_plugin@0.0.15/mod.ts";
-export { enableHelpersPlugin } from "https://deno.land/x/discordeno_helpers_plugin@0.0.8/mod.ts";
+export {
+  type BotWithHelpersPlugin,
+  enableHelpersPlugin,
+} from "https://deno.land/x/discordeno_helpers_plugin@0.0.8/mod.ts";
 
 // utils
 export * from "https://deno.land/std@0.117.0/fmt/colors.ts";
