6862a965ad6edaa9f35dbf908c8751595dfa1ec7
ThatGuyJamal <43446907+ThatGuyJamal@users.noreply.github.com>
2022-03-15 13:41:38 -0400

    Update mod.ts

--- core/providers/mod.ts
+++ core/providers/mod.ts
@@ -4,21 +4,17 @@ import { AkumaCreateBotOptions } from "../interfaces/Client.ts";
  * Base provider class for all providers.
  */
 export abstract class AkumaKodoProvider {
-  protected model: BaseProviderModel;
   protected logger: AkumaKodoLogger;
-  protected constructor(options: ProviderOptions, model: BaseProviderModel, config: AkumaCreateBotOptions) {
+  protected constructor(options: ProviderOptions, config: AkumaCreateBotOptions) {
     this.logger = new AkumaKodoLogger(config);
     if (options.provider === "mongodb") {
       if (!options.mongodb_connection_url) {
         throw new Error("MongoDB connection URL is required.");
       }
-      this.model = model;
       this.logger.create("info", "Provider", "MongoDB provider loaded!");
     } else if (options.provider === "postgres") {
-      this.model = model;
       this.logger.create("info", "Provider", "PostgreSQL provider loaded!");
     } else if (options.provider === "mysql") {
-      this.model = model;
       this.logger.create("info", "Provider", "MySQL provider loaded!");
     } else {
       throw new Error("Invalid provider type! Please use one of the following: mongodb, postgres, mysql");
