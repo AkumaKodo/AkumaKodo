530a8d0512738c225428d583df8ab578124984c9
ThatGuyJamal <43446907+ThatGuyJamal@users.noreply.github.com>
2022-03-15 03:36:54 -0400

    Update Command.ts

--- core/interfaces/Command.ts
+++ core/interfaces/Command.ts
@@ -1,23 +1,14 @@
 /** base command interface */
-import {
-  ApplicationCommandOption,
-  ApplicationCommandTypes,
-  DiscordenoInteraction,
-  DiscordenoMessage,
-  Permission,
-} from "../../deps.ts";
-import { ArgumentDefinition, ConvertArgumentDefinitionsToArgs } from "./Arugment.ts";
-import { AkumaKodoBotInterface } from "./Client.ts";
-import { AkumaKodoCollection } from "../lib/utils/Collection.ts";
+import { Permission } from "../../deps.ts";
 
 /** Base command interface. All commands have these options */
-export interface ParentCommand {
+export interface AkumaKodoCommand {
   /** The name of this command. */
-  name: string;
+  trigger: string;
   /** The command description */
   description?: string;
   /** Rate limits the command for users or the guild */
-  cooldown: cooldownInterface;
+  cooldown?: cooldownInterface;
   /**A list of member and role ids that can bypass the command cooldown*/
   ignoreCooldown?: bigint[];
   /**Checks if the member has the necessary roles to run the command*/
@@ -43,61 +34,9 @@ export interface ParentCommand {
   /** Runs the command function */
 }
 
-export interface MessageCommand<T extends readonly ArgumentDefinition[]> extends ParentCommand {
-  /** The command aliases. Do not work with slash commands. */
-  aliases?: string[];
-  /**The command arguments*/
-  arguments?: T;
-  /**A collection of subcommands*/
-  subcommands?: AkumaKodoCollection<string, Omit<MessageCommand<T>, "category">>;
-  execute: (
-    bot: AkumaKodoBotInterface,
-    message: DiscordenoMessage,
-    args: ConvertArgumentDefinitionsToArgs<T>,
-  ) => unknown | Promise<unknown>;
-}
-
-/** Used for making slash commands */
-export interface InteractionCommand extends ParentCommand {
-  /** The scope of the command. */
-  scope: "Guild" | "Global" | "Development";
-  /**A list of guild ids that will have the command*/
-  guildIds?: bigint[];
-  /**Whether the command is enabled by default when the app is added to a guild*/
-  defaultPermission?: boolean;
-  /**The application command type (context menus/input command)*/
-  type?: ApplicationCommandTypes;
-  /**A list of options for the command*/
-  options?: ApplicationCommandOption[];
-  /**A collection of */
-  subcommands?: AkumaKodoCollection<string, SlashSubcommandGroup | SlashSubcommand>;
-  execute?: (bot: AkumaKodoBotInterface, data: DiscordenoInteraction) => unknown | Promise<unknown>;
-}
-
-interface cooldownInterface {
+export interface cooldownInterface {
   /** How long the user needs to wait after the first execution until he can use the command again */
   seconds: number;
   /** How often the user is allowed to use the command until he is in cooldown */
   allowedUses: number;
-}
-
-/**The interface for slash subcommands*/
-export interface SlashSubcommand extends ParentCommand {
-  /**A list of options for the subcommand*/
-  options?: ApplicationCommandOption[];
-  /**The subcommand type*/
-  SubcommandType?: "subcommand";
-  execute?: (bot: AkumaKodoBotInterface, data: DiscordenoInteraction) => unknown | Promise<unknown>;
-}
-
-/**The interface for slash subcommands groups*/
-export interface SlashSubcommandGroup {
-  /**The subcommand group's name*/
-  name: string;
-  /**The subcommand group's description*/
-  description: string;
-  /**The subcommand type*/
-  SubcommandType?: "subcommandGroup";
-  /**A collection of subcommands for the group*/
-  subcommands?: AkumaKodoCollection<string, SlashSubcommand>;
 }
