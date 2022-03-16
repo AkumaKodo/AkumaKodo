30950ef09969317f266de624c83f3215e61c11b3
ThatGuyJamal <43446907+ThatGuyJamal@users.noreply.github.com>
2022-03-16 09:43:40 -0400

    Update mod.ts

--- mod.ts
+++ mod.ts
@@ -4,3 +4,4 @@ export * from "./core/AkumaKodo.ts";
 export { AkumaKodoMongodbProvider } from "./core/providers/mod.ts";
 export { AkumaKodoCollection } from "./core/lib/utils/Collection.ts"
 export {AkumaKodoEmbed} from "./core/lib/utils/Embed.ts";
+export {AkumaKodoTaskModule} from "./core/lib/task/mod.ts";
