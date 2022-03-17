7bcda3d6609b7da4bd39af52f591c43e207e8899
ThatGuyJamal <43446907+ThatGuyJamal@users.noreply.github.com>
2022-03-16 21:24:06 -0400

    AkumaArgumentGenorator

--- core/AkumaKodoController.ts
+++ core/AkumaKodoController.ts
@@ -1,15 +1,17 @@
 import { AkumaKodoBotCore } from "./AkumaKodo.ts";
 import { EventEmitter } from "../deps.ts";
+import {AkumaKodoCollection} from "./lib/utils/Collection.ts";
 
 export class AkumaKodoController<T extends AkumaKodoBotCore> extends EventEmitter {
   protected client: T;
   /* The directory of the modules */
   protected dir: string;
-  public pool: any;
+  public module: AkumaKodoCollection<string, any>
   public constructor(client: T, dir: string) {
     super();
     this.client = client;
     this.dir = dir;
+    this.module = new AkumaKodoCollection();
   }
 
   public async load(thing: any) {
@@ -21,13 +23,13 @@ export class AkumaKodoController<T extends AkumaKodoBotCore> extends EventEmitte
     return mod;
   }
   public remove(id: any) {
-    const mod = this.pool.get(id.toString());
+    const mod = this.module.get(id.toString());
     if (!mod) return;
     this.deregister(mod);
     return mod;
   }
   public async reload(id: any) {
-    const mod = this.pool.get(id);
+    const mod = this.module.get(id);
     if (!mod) return;
     this.deregister(mod);
 
@@ -35,14 +37,14 @@ export class AkumaKodoController<T extends AkumaKodoBotCore> extends EventEmitte
     return await this.load(filepath);
   }
   public deregister(mod: any) {
-    this.pool.delete(mod.id);
+    this.module.delete(mod.id);
   }
   public async reloadAll() {
-    for (const m of Array.from(this.pool.values()) as any[]) {
+    for (const m of Array.from(this.module.values()) as any[]) {
       if (m.filepath) await this.reload(m.id);
     }
 
-    return this.pool;
+    return this.module;
   }
   public async loadALL(dirPath?: any) {
     dirPath = await Deno.realPath(dirPath || this.dir);
@@ -61,6 +63,6 @@ export class AkumaKodoController<T extends AkumaKodoBotCore> extends EventEmitte
     mod.filepath = filepath;
     mod.handler = this;
     mod.client = this.client;
-    this.pool.set(mod.id, mod);
+    this.module.set(mod.id, mod);
   }
 }
--- core/interfaces/Argument.ts
+++ core/interfaces/Argument.ts
@@ -1,10 +1,11 @@
 // All the items the parsers return
 import {
-    ApplicationCommandOption,
+    ApplicationCommandOption, CreateMessage,
     DiscordenoChannel,
     DiscordenoMember,
     DiscordenoMessage,
-    DiscordenoRole
+    DiscordenoRole,
+    sendMessage
 } from "../../deps.ts";
 
 export type returnItems = DiscordenoRole | DiscordenoChannel | DiscordenoMember | number | string | boolean | undefined;
@@ -30,8 +31,7 @@ export const applyOptions = <T extends any>(options: T) =>
   };
 
 export interface AkumaKodoMessage extends DiscordenoMessage {
-    // TODO(#001) - implement
-   send: () => Promise<void>;
+   send: ( channelId: bigint,  content: CreateMessage) => Promise<AkumaKodoMessage>
 }
 
 export interface ConvertedOptions {
@@ -42,9 +42,9 @@ export interface prefixFn {
 }
 export interface ArgOptions extends ApplicationCommandOption {
     match?: Matches;
-    //The custom function that will be ran instead of the default argument function
+    //The custom function that will be run instead of the default argument function
     customType?: customType;
-    //The prompt that will be given when a user doesnt provide that argument
+    //The prompt that will be given when a user doesn't provide that argument
     prompt?: string;
     //If the option is a channel type, the channels shown will be restricted to these types
     channel_types?: string[];
@@ -55,7 +55,7 @@ export interface ArgOptions extends ApplicationCommandOption {
     //Enable autocomplete interactions for this option
     autocomplete?: boolean;
 }
-export type customType = (message: AkumaKodoMessage | DiscordenoMessage | any, content: string) => any | Promise<any>;
+export type customType = (message: AkumaKodoMessage | any, content: string) => any | Promise<any>;
 
 export enum Matches {
     rest,
