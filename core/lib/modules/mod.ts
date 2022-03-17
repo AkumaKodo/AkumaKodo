f729f940866880abc707c966da41b0458f17fffc
ThatGuyJamal <43446907+ThatGuyJamal@users.noreply.github.com>
2022-03-17 11:46:28 -0400

    Create mod.ts

---
+++ core/lib/modules/mod.ts
@@ -0,0 +1,17 @@
+import {AkumaKodoBotCore} from "../../AkumaKodo.ts";
+import {AkumaCreateBotOptions} from "../../interfaces/Client.ts";
+import {BotWithCache} from "https://deno.land/x/discordeno_cache_plugin@0.0.21/src/addCacheCollections.ts";
+
+
+export class AkumaKodoModule extends AkumaKodoBotCore {
+    protected internal: BotWithCache
+    public constructor(bot: BotWithCache, config: AkumaCreateBotOptions) {
+        super(config);
+        this.internal = bot
+        this.onLoad()
+    }
+
+    protected onLoad(): void {
+        this.container.logger.create("info", "AkumaKodo Module", "AkumaKodoModule has been initialized.");
+    }
+}
