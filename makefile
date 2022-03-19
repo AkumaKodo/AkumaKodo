041ba2aa43f5ca9d334e94406f5951255e9975eb
ThatGuyJamal <43446907+ThatGuyJamal@users.noreply.github.com>
2022-03-19 05:24:11 -0400

    Change logs

--- makefile
+++ makefile
@@ -2,3 +2,5 @@ test:
 	deno run -A ./test/__mock__.ts
 lib:
 	deno run -A ./mod.ts
+clg:
+	deno run -A ./scripts/clg.ts
---
+++ scripts/clg.ts
@@ -0,0 +1,22 @@
+import { Changelog, Release } from "https://deno.land/x/changelog@v2.0.0/mod.ts";
+
+/** Util to convert js into markdown for our change log files
+ *
+ * ? Options ~ Added, Changed, Deprecated, Removed, Fixed, and Security
+ */
+const changelog = new Changelog("AkumaKodo")
+  .addRelease(
+    new Release("0.1.0", "2022-3-19", "Initial release of AkumaKodo")
+      .added("Interaction Command handling")
+      .added("Lots of util functions")
+      .changed("Added internal slash command handling using events. You can now enable or disable this.")
+      .changed("Logger Errors to warn on un-stable modes.")
+      .removed(
+        "For now until slash commands are fully implemented, I will not focus on message based commands. You will have to implement them yourself for now.",
+      ),
+  )
+
+console.clear();
+console.log("\n- - - COPY THE GENERATED LOG BELOW - - -\n");
+console.log(changelog.toString());
+console.log("\n- - - COPY THE GENERATED LOG ABOVE - - -\n");
