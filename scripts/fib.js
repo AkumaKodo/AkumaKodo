5c38959821f92d31ff9db53dc43bc91863ff8183
ThatGuyJamal <43446907+ThatGuyJamal@users.noreply.github.com>
2022-03-23 18:14:59 -0400

    Update fib.js

--- scripts/fib.js
+++ scripts/fib.js
@@ -1,15 +1,23 @@
-(function () {
+(() => {
+  const loopCount = [];
   let x, y, z;
 
-  while (1) {
+  while (true) {
     x = 0;
     y = 1;
     do {
-      console.log(x);
+      console.log(`${x}`);
 
       z = x + y;
       x = y;
       y = z;
     } while (x < 255);
+
+    loopCount.push(x);
+
+    if (loopCount.length > 10_000) {
+      console.log(`Loop count: ${loopCount.length}. Ending...`);
+      break;
+    }
   }
-});
+})();
