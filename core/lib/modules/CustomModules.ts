aff12c079ed72a54e35e4c443b392ed20c714207
ThatGuyJamal <43446907+ThatGuyJamal@users.noreply.github.com>
2022-03-23 21:38:57 -0400

    Update CustomModules.ts

--- core/lib/modules/CustomModules.ts
+++ core/lib/modules/CustomModules.ts
@@ -10,18 +10,32 @@ export class AkumaKodoCustomModule {
   public configuration: AkumaKodoConfigurationInterface;
   private readonly instance: BotWithCache;
 
+  private _module_name: string
+
+  /**
+   * @param bot - The AkumaKodo client instance
+   * @param container - The AkumaKodo container instance
+   * @param config - The AkumaKodo configuration instance
+   * @param name - The name of the module
+   */
   public constructor(
     bot: BotWithCache,
     container: AkumaKodoContainerInterface,
     config: AkumaKodoConfigurationInterface,
+    name?: string
   ) {
     this.container = container;
     this.configuration = config;
     this.instance = bot;
+    this._module_name = name ?? "Custom Module";
     this.onLoad();
   }
 
+  /**
+   * The name of the custom module
+   * @param eventName 
+   */
   protected onLoad(): void {
-    this.container.logger.debug("info", "CustomModule", "Custom module loaded!");
+    this.container.logger.debug("info", "AkumaKodo Module" , `${this._module_name} loaded!`);
   }
 }
