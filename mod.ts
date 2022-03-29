d1da84273b93fbb02e21abbcddd56d6ba67a4c66
ThatGuyJamal <43446907+ThatGuyJamal@users.noreply.github.com>
2022-03-29 19:39:34 -0400

    Update mod.ts

--- mod.ts
+++ mod.ts
@@ -5,11 +5,17 @@ export { AkumaKodoMongodbProvider } from "./core/providers/mongodb.ts";
 export { AkumaKodoCollection } from "./core/lib/utils/Collection.ts";
 export { AkumaKodoEmbed } from "./core/lib/utils/Embed.ts";
 
+// Components
 export { Button } from "./core/lib/utils/Components/Button.ts";
 export { InputText } from "./core/lib/utils/Components/InputText.ts";
 export { SelectMenu } from "./core/lib/utils/Components/SelectMenu.ts";
+export { Components } from "./core/lib/utils/Components/mod.ts";
 
+// Utilities 
 export * from "./core/lib/utils/helpers.ts";
+
+// Typings
 export * from "./core/interfaces/Client.ts";
 export * from "./core/interfaces/Provider.ts";
 export * from "./core/interfaces/Command.ts";
+export type { ProviderOptions } from "./core/interfaces/Provider.ts"
