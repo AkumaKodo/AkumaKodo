314a15bbfee236c19d2359dd39739e38a9d37883
ThatGuyJamal <43446907+ThatGuyJamal@users.noreply.github.com>
2022-03-30 17:47:41 -0400

    misc

--- scripts/github_script.js
+++ scripts/github_script.js
@@ -3,6 +3,8 @@ const ghpages = require("gh-pages");
 ghpages.publish("./docs/book/html", {
     branch: "documentation",
     repo: "https://github.com/AkumaKodo/AkumaKodo.git",
+    message: "Documentation updated."
+// @ts-ignore - ignore the error
 }, function (err) {
     if (err) {
         console.log(err);
